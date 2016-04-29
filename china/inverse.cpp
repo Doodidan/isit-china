831a2a72c09ed684781ad67ad7530fe5a6953efc
GoshaEgoryan <gosha.egorian@gmail.com>
2016-04-29 15:55:58 +0300

    Подправка алгоритма (деление на 0)

--- china/inverse.cpp
+++ china/inverse.cpp
@@ -6,9 +6,15 @@ unsigned int inverse (unsigned int a, unsigned int b) {
 		return 1;
 	};
 
-	unsigned int b0 = b, t, q, x0 = 0, x1 = 1;
+	unsigned int b0 = b, t, x0 = 0, x1 = 1;
+	double q;
+
+	int i = 0;
 
 	while (a > 1) {
+		if (b == 0) {
+			break;
+		};
 		q = a / b;
 
 		t = b;
